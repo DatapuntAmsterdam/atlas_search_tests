682992e88ad507d8fe52720040d37b0a4ae5e741
Stephan Preeker <stephan@preeker.net>
2016-06-30 10:51:15 +0200

    add meetbouten to elk tests

--- tests/run_test_LOCAL.sh
+++ tests/run_test_LOCAL.sh
@@ -6,5 +6,5 @@ set -e   # stop on any error
 URL="http://127.0.0.1:8000"
 
 resttest.py $URL typeahead.yml
-#resttest.py $URL aanduidingen_test.yml
-#resttest.py $URL aanduidingen_test.yml
+resttest.py $URL smoke_test.yml
+resttest.py $URL aanduidingen_test.yml

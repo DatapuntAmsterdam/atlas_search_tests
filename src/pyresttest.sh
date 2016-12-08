36ba0219173fc95262f9eac618c38818a75b6b61
Jeroen van 't Hert <jeroen@yottabyte.io>
2016-12-08 14:32:40 +0100

    Add strict testing of HR endpoint

--- src/pyresttest.sh
+++ src/pyresttest.sh
@@ -7,5 +7,6 @@ URL=$1
 
 resttest.py ${URL} pyresttest/smoke_test.yml
 resttest.py ${URL} pyresttest/aanduidingen_test.yml
+resttest.py ${URL} pyresttest/hr_test.yml
 resttest.py ${URL} pyresttest/typeahead.yml
 
